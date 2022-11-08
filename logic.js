70a2c7ed60200dc6dfe2d7d4eb80c034e01e515a
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-08 16:47:36 +0100

    completed if else

--- logic.js
+++ logic.js
@@ -1,19 +1,33 @@
 // Calculation of the price of the train ticket
 
 
-
 const minPrice = 0.21;
 const firstTitle = document.getElementById("first-title");
-
-// const inputAge = document.getElementById("input-age");
 const button = document.querySelector("button");
+const inputKm = document.getElementById("input-km");
+const inputAge = document.getElementById("input-age");
+
 
 button.addEventListener("click",
 
   function(){
-    const inputKm = document.getElementById("input-km");
-    let price = inputKm * minPrice;
-    firstTitle.innerHTML = `Il prezzo del tuo biglietto è di ${price} euro.`;
-  }
+    let numKm = inputKm.value;
+    inputKm.value = null;
+    let numAge = inputAge.value;
+    inputAge.value = null;
+    let price = numKm * minPrice;
 
+    if (numAge < 18) { 
+        let reducedPrice = price * 80 / 100;
+        firstTitle.innerHTML = `Il prezzo del tuo biglietto è di ${price} euro, ma è scontato del 20% perchè hai meno di 18 anni. Quindi il prezzo del tuo biglietto è di ${reducedPrice} euro.`;
+        console.log("reduced price", reducedPrice);
+    } else if (numAge > 64) {
+        let reducedPrice = price * 60 / 100;
+        firstTitle.innerHTML = `Il prezzo del tuo biglietto è di ${price} euro, ma è scontato del 40% perchè hai più di 65 anni. Quindi il prezzo del tuo biglietto è di ${reducedPrice} euro.`;
+        console.log("reduced price", reducedPrice);
+    } else {
+        firstTitle.innerHTML = `Il prezzo del tuo biglietto è di ${price} euro.`;
+        console.log("price", price);
+    }    
+  }
 );
