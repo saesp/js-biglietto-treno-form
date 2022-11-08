975cb81fb64e417791b27c173e45cee3cf6106c9
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-08 17:30:45 +0100

    changes

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 // Calculation of the price of the train ticket
 
 
-const minPrice = 0.21;
+// const and let
 let namePass = document.getElementById("name-pass");
 let offerTic = document.getElementById("offer-tic");
 let priceTic = document.getElementById("price-tic");
@@ -12,29 +12,37 @@ const inputAge = document.getElementById("input-age");
 
 const button = document.querySelector("button");
 
+const minPrice = 0.21;
 
+
+// click
 button.addEventListener("click",
 
   function(){
+    // value
     let nameVal = inputName.value;
+    inputName.value = null;
     let numKm = inputKm.value;
     inputKm.value = null;
     let numAge = inputAge.value;
     inputAge.value = null;
     let price = numKm * minPrice;
 
-    if (numAge < 18) { 
+    // if else...
+    if (numAge < 18) { // <18 -20%
         let reducedPrice = price * 80 / 100;
         namePass.innerHTML = nameVal;
         offerTic.innerHTML = "- 20%";
         priceTic.innerHTML = `${reducedPrice} invece di ${price}`;
         console.log("reduced price", reducedPrice);
-    } else if (numAge > 64) {
+
+    } else if (numAge > 64) { // >64 -40%
         let reducedPrice = price * 60 / 100;
         namePass.innerHTML = inputName;
         offerTic.innerHTML = "- 40%";
         priceTic.innerHTML = `${reducedPrice} invece di ${price}`;
         console.log("reduced price", reducedPrice);
+
     } else {
         namePass.innerHTML = inputName;
         offerTic.innerHTML = "standart";
