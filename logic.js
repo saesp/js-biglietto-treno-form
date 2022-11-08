0d2c68eef844607d7652d240a338c07486e19696
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-08 16:09:55 +0100

    modifiche

--- logic.js
+++ logic.js
@@ -3,13 +3,13 @@
 
 
 const minPrice = 0.21;
-let price = inputKm * minPrice;
 const firstTitle = document.getElementById("first-title");
 const inputKm = document.getElementById("input-km");
-const inputAge = document.getElementById("input-age");
-const button = document.getElementById("button");
+// const inputAge = document.getElementById("input-age");
+const button = document.querySelector("button");
+let price = inputKm * minPrice;
 
-button.addEventListener('click',
+button.addEventListener("click",
 
   function(){
     firstTitle.innerHTML = `Il prezzo del tuo biglietto è di ${price} euro.`;
